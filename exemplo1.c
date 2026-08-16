8757f0c1a66096d9d940b519e42172fce3b1a514
moisessprochabr-art <moisessprochabr@gmail.com>
2026-08-16 19:13:14 +0000

    CC MOISÉS

--- exemplo1.c
+++ exemplo1.c
@@ -3,13 +3,10 @@
 
 #include <stdio.h>
 
-int main(){
-  //imprime algumas mensagens na tela
-  printf("Mackenzie - FCI\n");
-  printf("Algoritmos e ");
-  printf("Prog. II\n");
-
-
-
+  int main(){
+    //imprime algumas mensagens na tela
+    printf("Mackenzie - FCI\n");
+    printf("Ciência da Computação\n");
+    printf("Algoritmos e Programação II\n");
   return 0;
 }
